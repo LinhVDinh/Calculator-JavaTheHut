29d43209bf5f693d1281d4cccb0914a8b238f568
Linh <Linh@10.0.0.207>
2022-06-14 20:51:59 -0700

    First Commit

---
+++ Lab02/src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("220065431 - This line was added by Linh Dinh.");
+		System.out.println("302676361 - This line was added by Francesca Del Rosario.");
+		System.out.println("301584309 - This line was added by Keegan Rhoads.");
+		System.out.println("219822019 - This line was added by Kevin Deol.");
+		System.out.println("220142365 - This line was added by David Castrejon.");
+		System.out.println("219991773 - This line was added by Khushleen Dhami.");
+
+}
+
+}
